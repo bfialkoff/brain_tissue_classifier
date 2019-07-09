4c5cc4d6620e3115a8a0ae4c6494f4865a8928e5
bfialkoff <39707315+bfialkoff@users.noreply.github.com>
2019-07-09 10:11:39 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='brain_tissue_classifier',
+   version='1.0',
+   description='A useful module',
+   author='B F',
+   author_email='foomail@foo.com',
+   packages=['brain_tissue_classifier'],  #same as name
+)
